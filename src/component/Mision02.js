649dc39525d0c0b0d6f690bca54bd7214e04355d
David Garcia <david.garcia@pranical.com>
2024-10-06 23:53:26 -0400

    iteration mision2

--- src/component/Mision02.js
+++ src/component/Mision02.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const Mision02 = ({ name }) => {
+  const backgroundStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/img/fondo_2.svg) !important`,
+    backgroundSize: 'cover', // O 'contain', según tus necesidades
+    height: '100vh', // Asegúrate de que tenga altura para ver el fondo
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundRepeat: 'no-repeat',
+    animation: 'movimientoFondo 35s infinite alternate'
+
+};
   return (
-    <div className="container_welcome">
+    <div className="" style={backgroundStyle}>
       <h2>¡Hello, {name}!</h2>
       <br/  >
       <p>¡my name is Alex, I am at international Space Station</p>
